368b7f93ed074bbfc80b44c2cba1fe356b7045df
olgaKhristo <olga84h@gmail.com>
2023-01-06 15:06:55 +0000

    main

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,39 @@
 <template>
-  <img ">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+ <h1>Reaction timer</h1>
+ <button @click="start" :disabled="isPlaying">Play</button>
+ <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+ <Results v-if="showResults" :score="score"/>
+ 
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+<script>
+import Block from "./components/block.vue"
+import Results from "./components/results.vue"
+export default{
+  name: 'App',
+  components: {Block, Results},
+  data(){
+    return{
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
 
-@Options({
-  components: {
-    HelloWorld,
+    }
   },
-})
-export default class App extends Vue {}
+  methods: {
+    start(){
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      this.showResults = false
+     },
+     endGame(reactionTime){
+this.score = reactionTime
+this.isPlaying = false
+this.showResults = true
+     }
+   }
+  }
 </script>
 
 <style>
@@ -21,7 +42,23 @@ export default class App extends Vue {}
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
+}
+
+button{
+  background: aqua;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction time: {{score}} ms</p>
+  <p class="rank">{{rank}}</p>
+</template>
+
+<script>
+export default {
+props: ['score'],
+data(){
+    return{
+        rank: null
+    }
+},
+mounted(){
+    if (this.score < 250) {
+        this.rank = 'fast fingers'
+    } else if(this.score < 400) {
+        this.rank = 'rapid reflex'
+    } else {
+        this.rank = 'Snail pace'
+    }
+ }
+}
+</script>
+
+<style>
+.rank{
+    font-size: 1.4rem;
+    color:aqua;
+    font-weight: bold;
+}
+</style>
